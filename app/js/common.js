3b7b39cb0942bde83752292fe3c55a6fdc3746d6
JesterTS <lysov1995@gmail.com>
2018-12-25 04:14:13 +0300

    Day 6.2

--- app/js/common.js
+++ app/js/common.js
@@ -32,7 +32,25 @@ function initMap() {
         icon: "img/icon/map.png"             // (Необязательно) Путь до изображения, которое будет выводится на карте вместо стандартного маркера
     });
 }
+function CityinitMap(LatId, lngId,th) {
 
+    var centerLatLng = new google.maps.LatLng(LatId, lngId);
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 16
+    };
+ 
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+    // Добавляем маркер
+    var marker = new google.maps.Marker({
+        position: centerLatLng,               // Координаты расположения маркера. В данном случае координаты нашего маркера совпадают с центром карты, но разумеется нам никто не мешает создать отдельную переменную и туда поместить другие координаты.
+        map: map,                             // Карта на которую нужно добавить маркер
+        title: "Текст всплывающей подсказки", // (Необязательно) Текст выводимый в момент наведения на маркер,
+        icon: "img/icon/map.png"             // (Необязательно) Путь до изображения, которое будет выводится на карте вместо стандартного маркера
+    });
+    google.maps.event.addDomListener(window, "load", marker);
+} 
 function CustominitMap(LatId, lngId,th) {
     $('.map_link').removeClass('active');
     $(th).addClass('active');
